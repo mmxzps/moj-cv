accfac20ab4dbd9e596105565a6303fe3abf31ee
Mojtaba <99285003+mmxzps@users.noreply.github.com>
2024-11-28 20:39:54 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
 
             <ul>
             <li style={{ transform: "none" }}><h2>Contact me</h2></li>
-              <li></li>
+              <li>-</li>
               <li><a href="mailto:mojtaba.mobasheri@hotmail.com">mojtaba.mobasheri@hotmail.com</a></li>
             </ul>
   
